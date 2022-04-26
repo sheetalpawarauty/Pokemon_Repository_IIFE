aeded1c521a0927f743b6328922d69f10b75caa2
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-04-26 10:40:25 +0200

    Update scripts.js
    If applied, this commit will have pokemonList Array wrapped in an IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
     { name: 'pikachu', height: 4, types: ['field','fairy'] },
     { name: 'bulbasaur', height: 7, types: ['monster','grass'] },
     { name: 'paras', height: 3, types: ['bug','grass'] },
     { name: 'seel', height: 11, types: ['water','field'] },
     { name: 'squirtle', height: 5, types: ['monster','water'] }
   ];
+})();
 
   document.write("Pokemon Array List:");
   document.write("<ul>");
